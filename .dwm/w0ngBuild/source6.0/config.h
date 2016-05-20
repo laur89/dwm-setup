laur89/dwm-setup

commit d61148d7cd5c90140388179daea12dc0b476c7e9

add client_class_idea_loading_dialog to ignored_class_list[]

.dwm/w0ngBuild/.tags
@@ -73,6 +73,7 @@ clients	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/dwm.c
 client_class_copyq	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_copyq[] = "copyq";$/;"	v	language:C++
 client_class_gsimplecal	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_gsimplecal[] = "gsimplecal";$/;"	v	language:C++
 client_class_idea	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_idea[] = "sun-awt-X11-XFramePeer";$/;"	v	language:C++
+client_class_idea_loading_dialog	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_idea_loading_dialog[] = "sun-awt-X11-XDialogPeer";$/;"	v	language:C++
 client_class_notifyd	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_notifyd[] = "xfce4-notifyd";$/;"	v	language:C++
 client_class_screenkey	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_screenkey[] = "screenkey";$/;"	v	language:C++
 ClkClientWin	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/dwm.c	/^       ClkClientWin, ClkRootWin, ClkLast };             \/* clicks *\/$/;"	e	language:C++	enum:__anon7	file:

.dwm/w0ngBuild/source6.0/config.h
@@ -435,6 +435,7 @@ static Button buttons[] = {
  *Window class definitions:
  */
 const char client_class_idea[] = "sun-awt-X11-XFramePeer";
+const char client_class_idea_loading_dialog[] = "sun-awt-X11-XDialogPeer";
 const char client_class_notifyd[] = "xfce4-notifyd";
 const char client_class_gsimplecal[] = "gsimplecal";
 const char client_class_screenkey[] = "screenkey";
@@ -447,7 +448,8 @@ const char client_class_copyq[] = "copyq";
 const char *ignored_class_list[] = {
     client_class_notifyd,
     client_class_gsimplecal,
-    client_class_screenkey
+    client_class_screenkey,
+    client_class_idea_loading_dialog
 };
 
 // synergy logfile location: // TODO: delete

commit d71967ace0f6e556ccd22e4844a22f343ee34aea

reverts d61148d which caused xserver to fail on startup

.dwm/w0ngBuild/.tags
@@ -73,7 +73,6 @@ clients	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/dwm.c
 client_class_copyq	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_copyq[] = "copyq";$/;"	v	language:C++
 client_class_gsimplecal	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_gsimplecal[] = "gsimplecal";$/;"	v	language:C++
 client_class_idea	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_idea[] = "sun-awt-X11-XFramePeer";$/;"	v	language:C++
-client_class_idea_dialog	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_idea_dialog[] = "sun-awt-X11-XDialogPeer";$/;"	v	language:C++
 client_class_notifyd	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_notifyd[] = "xfce4-notifyd";$/;"	v	language:C++
 client_class_screenkey	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/config.h	/^const char client_class_screenkey[] = "screenkey";$/;"	v	language:C++
 ClkClientWin	/data/progs/deps/homesick/repos/dwm-setup/.dwm/w0ngBuild/source6.0/dwm.c	/^       ClkClientWin, ClkRootWin, ClkLast };             \/* clicks *\/$/;"	e	language:C++	enum:__anon7	file:

.dwm/w0ngBuild/source6.0/config.h
@@ -435,7 +435,7 @@ static Button buttons[] = {
  *Window class definitions:
  */
 const char client_class_idea[] = "sun-awt-X11-XFramePeer";
-const char client_class_idea_dialog[] = "sun-awt-X11-XDialogPeer";
+//const char client_class_idea_dialog[] = "sun-awt-X11-XDialogPeer";
 const char client_class_notifyd[] = "xfce4-notifyd";
 const char client_class_gsimplecal[] = "gsimplecal";
 const char client_class_screenkey[] = "screenkey";
@@ -448,8 +448,7 @@ const char client_class_copyq[] = "copyq";
 const char *ignored_class_list[] = {
     client_class_notifyd,
     client_class_gsimplecal,
-    client_class_screenkey,
-    client_class_idea_dialog
+    client_class_screenkey
 };
 
 // synergy logfile location: // TODO: delete